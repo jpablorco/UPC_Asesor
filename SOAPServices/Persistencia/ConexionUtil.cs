127f7272790b0900118bc982c1d9df79344b8b76
jpablorco <jpablo.rco@gmail.com>
2013-09-18 22:37:47 -0500

    Primera Subida

---
+++ SOAPServices/Persistencia/ConexionUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOAPServices.Persistencia
+{
+    public class ConexionUtil
+    {
+
+        public static string ObtenerCadena()
+        {
+            return "Data Source=(local);Initial Catalog=BD_Asesores;Integrated Security=SSPI";
+        }
+    }
+}
